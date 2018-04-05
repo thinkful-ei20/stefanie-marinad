1af99eca6bcfe66611cd2215486c1170c2340cc4
Stefanie Carrasco <stefanieanncarrasco@gmail.com>
2018-04-05 15:32:03 -0400

    drills

--- index.js
+++ index.js
@@ -2,19 +2,32 @@
 /* global $ */
 
 function main() {
-  let bigImageSrc = $('.js-big-image').attr('src');
-  let bigImageAlt = $('.js-big-image').attr('alt');
- 
-  $('.thumbnails').on('click', 'a', event => {
-    // let littleImageSrc = $(this).find('img').attr('src');
-    let littleImageSrc = $(this).children().attr('src');
-    let littleImageAlt = $(this).children().attr('alt');
-    console.log(littleImageAlt);
-    console.log(littleImageSrc);
-
-    $('.js-big-image').attr('src', littleImageSrc);
-    $('.js-big-image').attr('alt', littleImageAlt);
+  $('.thumbnail').on('click', event => {
+    const target = $(event.target);
+    handleTrigger(target, event);
+    console.log($(this));
   });
+
+
+  $('.thumbnail').on('keydown', event => {
+    if (event.which === 13) {
+      const target = $(event.target).children();
+      console.log(target);
+      handleTrigger(target, event);
+      //console.log(event.which);
+    } 
+  });
+}
+
+
+
+function handleTrigger(elem, event) {
+  //console.log(event.target);
+  event.preventDefault();
+  const littleImageSrc = elem.attr('src');
+  const littleImageAlt = elem.attr('alt');
+  $('.js-big-image').attr('src', littleImageSrc);
+  $('.js-big-image').attr('alt', littleImageAlt);
 }
 
 $(main);
